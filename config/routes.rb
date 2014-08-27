6924f652e77e63951b66bc28c597ac3cfb3e89a3
Tom Brennan <tommybrennan@gmail.com>
2014-08-27 17:19:04 -0400

    created the shipment resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ShippingRates::Application.routes.draw do
+  resources :shipments
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
