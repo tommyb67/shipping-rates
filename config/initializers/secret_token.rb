08a12af498d219e18cd152d9eac29b61a9e497a7
Tom Brennan <tommybrennan@gmail.com>
2014-08-27 17:13:11 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShippingRates::Application.config.secret_key_base = 'a87120166184d22a9fb019e4b609d4dd589f0aafde4bf18bb4120ec8c797e24b7e0f35c9b3e91934228b5aef6f1b8098cf0ced5e55bcde0e1666f0f7634f3a42'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ShippingRates::Application.config.session_store :cookie_store, key: '_shipping_rates_session'
